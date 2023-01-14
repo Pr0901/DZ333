2bf362c8c9bc4a38961d28af5dc0fd885e41020f
Sofya <sofa33158@gmail.com>
2023-01-14 13:08:37 +0300

    Two

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         int initialAccount = 100;
         int payment = 1100;
         int bonus;
-            if (payment > 1000) {
-                bonus = payment / 100;
-            } else {
-                bonus = 0;
-            }
-            int finalAccount = initialAccount + payment + bonus;
-
-            System.out.println("На счету:" + finalAccount);
-
+        if (payment > 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
         }
-        }
+        int finalAccount = initialAccount + payment + bonus;
+
+        System.out.println("На счету:" + finalAccount);
+
+    }
+}
